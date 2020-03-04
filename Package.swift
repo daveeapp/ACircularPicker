9b6b1d116fc2c167290cb607a8ac459d2995da77
davee <daveeapp@gmail.com>
2020-03-05 03:45:05 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,24 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "ACircularPicker",
+    platforms: [
+        .iOS(.v10)
+    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "ACircularPicker",
-            targets: ["ACircularPicker"]),
+        .library(name: "ACircularPicker", targets: ["ACircularPicker"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "ACircularPicker",
-            dependencies: []),
+            name: "ACircularPicker",path: "Source"),
         .testTarget(
             name: "ACircularPickerTests",
             dependencies: ["ACircularPicker"]),
+    ],
+    swiftLanguageVersions: [
+        .v5
     ]
 )
